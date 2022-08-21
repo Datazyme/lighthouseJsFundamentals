79a8a6e1818d57cff574b15906fbc16ce657dc47
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-08-21 23:33:09 +0000

    The whichSchool exersise

---
+++ whichSchool.js
@@ -0,0 +1,11 @@
+let age = 15
+let whichSchool = function (age) {
+  if (age < 13 ) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else if (age > 18) {
+    return "Lighthouse Labs";
+  }
+}
+console.log (whichSchool(14));
