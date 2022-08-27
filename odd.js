cfe695fe9e8612c0682a7d7d736a211d97a8fcb2
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-08-27 01:47:31 +0000

    finding odd numbers

---
+++ odd.js
@@ -0,0 +1,11 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
+
+const isOdd = function (num) {
+  return num % 2 > 0;
+}
+console.log(isOdd(5));
