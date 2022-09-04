7f59d1a0e210fdc3d5a4168c93c766e093fc199d
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-09-04 23:04:54 +0000

    objects and functions

---
+++ weRootingForYou.js
@@ -0,0 +1,30 @@
+const vegetables = [
+    {
+      submitter: 'Old Man Franklin',
+      redness: 10,
+      plumpness: 5
+    },
+    {
+      submitter: 'Sally Tomato-Grower',
+      redness: 2,
+      plumpness: 8
+    },
+    {
+      submitter: 'Hamid Hamidson',
+      redness: 4,
+      plumpness: 3
+    }
+  ]
+    
+const metric = 'redness'
+
+const judgeVegetable = function (vegetables, metric) {
+    let bestVeggie = vegetables[0];
+    for (let i = 0; i < vegetables.length; i++) {
+      if (vegetables[i][metric] > bestVeggie[metric]) {
+        bestVeggie = vegetables[i]
+      }
+    }
+    return bestVeggie.submitter;
+  }
+  console.log(judgeVegetable(vegetables, metric));
